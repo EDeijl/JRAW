f5fd9410d4e0ea06612a3f732ce6b0103ae26ec3
Erik Deijl <erik.deijl@gmail.com>
2013-06-06 12:00:44 +0200

    testNG

---
+++ test/RedditTest.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+/**
+ *
+ * User: Erik Deijl
+ * Date: 06-06-13
+ * Time: 11:51
+ *
+ *
+ */
+public class RedditTest
+{
+    @Test
+    public void testCreateGet() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testCreatePost() throws Exception
+    {
+
+    }
+}
